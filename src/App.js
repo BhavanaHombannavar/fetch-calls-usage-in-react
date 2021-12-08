8db0e7e8418090396822e519a316b7288a3cc29b
Bhavana Hombannavar <h.bhavana@webandappstudio.com>
2021-12-08 21:42:37 +0530

    fetch call example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [updateApp, setUpdateApp] = useState(false);
+
+  useEffect(()=>{
+    let currentPosts = posts;
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(json=>json.json())
+    .then(jsonRes=>{
+      jsonRes.map((obj)=>{
+        // console.log(obj)
+        return fetch('https://jsonplaceholder.typicode.com/photos/'+obj.id)
+        .then(res=>res.json())
+        .then(photo=>{
+          // console.log(obj)
+          // console.log(photo)
+
+          currentPosts.push({obj, photo})
+          setPosts(currentPosts)
+        })
+      })
+    })
+    .then(()=>{
+      setUpdateApp(true)
+    })
+  }, [updateApp])
+
+  const changeColor = (e) => {
+    console.log(e)
+    e.target.classList.add('active')
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"  >
+      {console.log(posts)}
+      {posts.map(post=>{
+        return <div className="width100" key={post.id}>
+          <div className="width30">
+              <img src={post.photo.thumbnailUrl} />
+          </div>
+          <div className="width70">
+            <h2 onClick={changeColor}>{post.obj.title}</h2>
+            {post.obj.body.length > 150 ? post.obj.body.slice(0, 150)+'...'  : post.obj.body}
+          </div>
+        </div>
+      })}
+
     </div>
   );
 }
